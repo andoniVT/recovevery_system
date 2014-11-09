782ee2fa8b49a14f95be24cfd332464cfdb45e0b
andoniVT <andoni.valverde@ucsp.edu.pe>
2014-11-09 09:52:54 -0500

    modifiend test

--- Preprocesser/prueba.py
+++ Preprocesser/prueba.py
@@ -4,4 +4,20 @@ Created on 09/11/2014
 @author: andoni
 '''
 
-print "hello"
+import cPickle
+
+vec = [0 , 1, 1 , 1 , 0 , 0]
+vec2 = [1 , 1, 0 , 1 , 1 , 0]
+vec3 = [0 , 0, 0 , 1 , 1 , 0]
+
+matrix = [vec , vec2 , vec3]
+
+with open("file.pk1" , 'wb') as fid:
+    cPickle.dump(matrix , fid)
+
+with open("file.pk1" , 'rb') as fid:
+    copia = cPickle.load(fid)
+    
+
+for i in copia:
+    print i 
