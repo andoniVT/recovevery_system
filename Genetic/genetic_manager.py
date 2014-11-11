c8ad608453d697111a9f9052542d37c98680ba4c
andoniVT <andoni.valverde@ucsp.edu.pe>
2014-11-10 19:24:43 -0500

    genetic

---
+++ Genetic/genetic_manager.py
@@ -0,0 +1,22 @@
+'''
+Created on 10/11/2014
+
+@author: andoni
+'''
+
+'''
+numero de individuos   : 
+numero de generaciones :
+numero experimentos    :
+numero de variables    :
+tipo de cruce          :
+probabilidad de cruce  :
+probabilidad  mutacion :
+aplicar elitismo       :
+precision              :
+limite superior        :
+limite inferior        :
+funcion de evaluacion  : 
+
+'''
+
